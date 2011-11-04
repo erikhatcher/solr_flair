65d5df9e04b0e3d3a1ac76d7881488b540fed822
Erik Hatcher <erik.hatcher@lucidimagination.com>
2011-11-04 08:26:33 -0400

    initial structure, empty Rails app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SolrFlair::Application.config.secret_token = '5bd4df360fcc5ad42704caa5c5a15c5c6aa256cd09978a95afe02ef6c202386a936d3e3eeff5a4ea048ebeae68677c438afd8cec6dafc6fb4a5e5b0b998c7566'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SolrFlair::Application.config.session_store :cookie_store, :key => '_solr_flair_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SolrFlair::Application.config.session_store :active_record_store
